5c706b002181bda6bf12fe5b42f075c37d6e43ea
natansouza5 <103135468+natansouza5@users.noreply.github.com>
2022-06-28 15:56:10 -0300

    correção de erro

--- Atividade/Atividade.js
+++ Atividade/Atividade.js
@@ -9,55 +9,50 @@ let data = new Date()
 let dia = data.getDate()
 let mes = data.getMonth() + 1 //o getmonth vai 0 à 11, então para não ficar confuso adicionei o +1 :)
 let ano = data.getFullYear()
-
+//**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**==**//
 let idade = 18
 if (idade < 18) {
-  console.log('Você precisa ter mais de 18 anos para criar um evento')
+  console.log('Você precisa ter 18 anos ou mais para criar um evento')
 } else {
   //data do evento
-  let eventoDia = 30
-  let eventoMes = 08
-  let eventoAno = 2022
+  let eventoDia = 22
+  let eventoMes = 01
+  let eventoAno = 2023
   // processamento/saída
-  if (eventoAno >= ano) {
-    if (eventoMes >= mes) {
-      if (eventoDia >= dia) {
-        console.log(`Evento pode ser realizado!`)
-        let convidados = [
-          'Pedro',
-          'Maria',
-          'José',
-          'Luis',
-          'Sandra',
-          'Adriana',
-          'Manoel',
-          'Geraldo',
-          'Márcia',
-          'Jorge',
-          'Marcelo'
-        ]
-        let palestrantes = ['Carlos', 'Francisca', 'Manoel']
-        let participantes = convidados.length + palestrantes.length
-        if (participantes == 100) {
-          console.log(
-            'Você alcançou o limite de 100 participantes, você não pode adicionar mais participantes!'
-          )
-        } else if (participantes > 100) {
-          console.log(
-            `limite de 100 pessoas excedido, por favor remover ${
-              participantes - 100
-            } participantes do evento!`
-          )
-        } else {
-          console.log(`Restam ${100 - participantes} vagas para participantes.`)
-        }
-      } else {
-        console.log(`dia inválido!`)
-      }
+  if (
+    (eventoAno == ano && eventoMes >= mes && eventoDia >= dia) ||
+    eventoAno > ano
+  ) {
+    console.log('O evento está agendado!')
+    let convidados = [
+      'Pedro',
+      'Maria',
+      'José',
+      'Luis',
+      'Sandra',
+      'Adriana',
+      'Manoel',
+      'Geraldo',
+      'Márcia',
+      'Jorge',
+      'Marcelo'
+    ]
+    let palestrantes = ['Carlos', 'Francisca', 'Manoel']
+    let participantes = convidados.length + palestrantes.length
+    if (participantes == 100) {
+      console.log(
+        'Você alcançou o limite de 100 participantes, você não pode adicionar mais participantes!'
+      )
+    } else if (participantes > 100) {
+      console.log(
+        `limite de 100 pessoas excedido, por favor remover ${
+          participantes - 100
+        } participantes do evento!`
+      )
     } else {
-      console.log(`mês inválido`)
+      console.log(`Restam ${100 - participantes} vagas para participantes.`)
     }
   } else {
-    console.log(`ano inválido`)
+    console.log('data inválida')
   }
 }
